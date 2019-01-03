c3dbdf3ae1dd9fff08891e9a17f588aaee96598f
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-01-03 17:08:07 +0600

    Second HM

---
+++ source/repos_c#/DZ_3_3/DZ_3_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*3.	Числовые значения символов нижнего регистра в коде ASCII отличаются от значений символов верхнего регистра на величину 32.
+             * Используя эту информацию, написать программу, которая считывает с клавиатуры и конвертирует все символы нижнего регистра в символы верхнего регистра и наоборот*/
+
+            int spaceSymbol = 0;
+            ConsoleKeyInfo symbol;
+            do
+            {
+                Console.WriteLine("Введите символ для конвертации регистра ");
+                Console.WriteLine("Для окончания ввода нажм ESC ");
+                symbol = Console.ReadKey();
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("Введенный символ " + symbol.KeyChar);
+
+                if (char.IsLetter(Convert.ToChar(symbol.KeyChar)))
+                {
+                    if (char.IsUpper(Convert.ToChar(symbol.KeyChar)))
+                    {
+                        Console.WriteLine("Конвертируемый символ " + char.ToLower(Convert.ToChar(symbol.KeyChar)));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Конвертируемый символ " + char.ToUpper(Convert.ToChar(symbol.KeyChar)));
+                    }
+                }
+
+                if(Convert.ToInt32(symbol.KeyChar) == 27)
+                {
+                    break;
+                }
+
+            } while (true);
+        }
+    }
+}
